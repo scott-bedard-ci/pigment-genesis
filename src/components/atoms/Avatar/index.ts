da1b2dfc8c83216a5a52bdd5ea69ed42d6f9d471
scott-bedard-ci <scott.bedard@customink.com>
2025-05-23 16:36:10 -0700

    starting over

--- src/components/atoms/Avatar/index.ts
+++
@@ -1,4 +0,0 @@
-export { Avatar } from './Avatar';
-export { AvatarGroup } from './AvatarGroup';
-export type { AvatarProps } from './Avatar';
-export type { AvatarGroupProps } from './AvatarGroup';
